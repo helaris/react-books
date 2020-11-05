81613563e88c37ebbbae129572788e8393c741fc
Helari <helari.sosi@gmail.com>
2020-11-05 15:16:52 +0100

    Add header and searchbar styling

--- client/src/components/Search/Searchbar.js
+++ client/src/components/Search/Searchbar.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './Seachbar.css';
 
 function Searchbar({ handleChange }) {
   const [input, setInput] = useState('');
-
   const handlingChange = (e) => {
     setInput(e.target.value);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     handleChange(input);
@@ -14,13 +17,19 @@ function Searchbar({ handleChange }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <TextField
+        width="75%"
+        id="outlined-search"
+        label="Search field"
+        variant="outlined"
         className="input"
         type="text"
         value={input}
         onChange={handlingChange}
       />
-      <button onClick={handleSubmit}>Search</button>
+      <Button onClick={handleSubmit} className="search__btn" variant="contained" color="primary" size="large">
+        Search
+      </Button>
     </form>
   )
 }
